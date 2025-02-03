c858b044e4cd0cff7c2ca43c274c8b4ea0059489
laurhakob <laurhakob@gmail.com>
2025-02-03 14:45:18 +0400

    upstash redis setup

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -5,37 +5,45 @@ import { db } from "@/database/drizzle";
 import { users } from "@/database/schema";
 import { hash } from "bcryptjs";
 import { signIn } from "@/auth";
-
-
+import { headers } from "next/headers";
+import ratelimit from "../ratelimit";
+import { redirect } from "next/navigation";
 
 export const signInWithCredentials = async (
-    params: Pick<AuthCredentials, "email" | "password">,
-  ) => {
-    const { email, password } = params;
-  
-  
-    try {
-      const result = await signIn("credentials", {
-        email,
-        password,
-        redirect: false,
-      });
-  
-      if (result?.error) {
-        return { success: false, error: result.error };
-      }
-  
-      return { success: true };
-    } catch (error) {
-      console.log(error, "Signin error");
-      return { success: false, error: "Signin error" };
+  params: Pick<AuthCredentials, "email" | "password">
+) => {
+  const { email, password } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) return redirect("/too-fast");
+
+  try {
+    const result = await signIn("credentials", {
+      email,
+      password,
+      redirect: false,
+    });
+
+    if (result?.error) {
+      return { success: false, error: result.error };
     }
-  };
 
-
+    return { success: true };
+  } catch (error) {
+    console.log(error, "Signin error");
+    return { success: false, error: "Signin error" };
+  }
+};
 
 export const signUp = async (params: AuthCredentials) => {
   const { fullName, email, universityId, password, universityCard } = params;
+
+  const ip = (await headers()).get("x-forwarded-for") || "127.0.0.1";
+  const { success } = await ratelimit.limit(ip);
+
+  if (!success) return redirect("/too-fast");
 
   const existingUser = await db
     .select()
@@ -58,7 +66,7 @@ export const signUp = async (params: AuthCredentials) => {
       universityCard,
     });
 
-     await signInWithCredentials({ email, password });
+    await signInWithCredentials({ email, password });
 
     return { success: true };
   } catch (error) {
--- lib/config.ts
+++ lib/config.ts
@@ -7,7 +7,11 @@ const config = {
       privateKey: process.env.IMAGEKIT_PRIVATE_KEY!,
     },
 
-    databaseUrl: process.env.DATABASE_URL!
+    databaseUrl: process.env.DATABASE_URL!,
+    upstash: {
+      redisUrl: process.env.UPSTASH_REDIS_URL,
+      redisToken: process.env.UPSTASH_REDIS_TOKEN,
+    },
   },
 };
 
