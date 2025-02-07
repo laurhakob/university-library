1eb0e06913e994341a9a3dbb5f50ba69288a9e25
laurhakob <laurhakob@gmail.com>
2025-02-07 11:59:57 +0400

    borrow functionality

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,10 @@
-"use client";
-
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
-import { cn, getInitials } from "@/lib/utils";
-import { Session } from "next-auth";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
 import React from "react";
+import { Button } from "./ui/button";
+import { signOut } from "@/auth";
 
-const Header = ({ session }: { session: Session }) => {
-  const pathname = usePathname();
-
+const Header = () => {
   return (
     <header className="my-10 flex justify-between gap-5 ">
       <Link href="/">
@@ -20,25 +13,24 @@ const Header = ({ session }: { session: Session }) => {
 
       <ul className="flex flex-row items-center gap-8">
         <li>
-          <Link
-            href="/library"
-            className={cn(
-              "text-base cursor-pointer capitalize",
-              pathname === "/library" ? "text-light-200" : "text-light-100"
-            )}
-          >
-            Library
-          </Link>
-        </li>
+          <form
+            action={async () => {
+              "use server";
 
-        <li>
-          <Link href="/my-profile">
+              await signOut();
+            }}
+            className="mb-10"
+          >
+            <Button>Logout</Button>
+          </form>
+
+          {/* <Link href="/my-profile">
             <Avatar>
               <AvatarFallback className="bg-amber-100">
                 {getInitials(session?.user?.name || "IN")}
               </AvatarFallback>
             </Avatar>
-          </Link>
+          </Link> */}
         </li>
       </ul>
     </header>
